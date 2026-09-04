b094d8cf1c90baa6487fb7a956e8306fea51d509
Naomi Veliz <solanch.veliz@tecsup.edu.pe>
2026-09-04 12:08:03 -0500

    commit:1

---
+++ Basico/BASICO-EJE1.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let amistad = "Hola"
+let nombre = "Matias"
+let edad = 24
+print("\(amistad) \(nombre) tu edad es \(edad) años y te amo mucho")
+
+
---
+++ Basico/Basico/Basico/BasicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicoApp.swift
+//  Basico
+//
+//  Created by Naomi Solanch Veliz Pie on 4/09/26.
+//
+
+import SwiftUI
+
+@main
+struct BasicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
